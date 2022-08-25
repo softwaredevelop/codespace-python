ab91a4d23205b3f7fec134f646fd42077598e671
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-08-25 08:22:20 +0000

    test: Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -35,5 +35,16 @@ check "shfmt" shfmt --version
 check "shellcheck" shellcheck --version
 check "editorconfig" ec --version
 check "trivy" trivy --version
+if [ "${ID_OS}" = "ubuntu" ]; then
+  python_system_package_list_debian="python3 \
+        python3-dev \
+        python3-pip \
+        python3-venv"
+  checkOSPackages "python-system-packages" ${python_system_package_list_debian}
+fi
+check "python" python --version
+check "python3" python3 --version
+check "python3 ascii art" python3 -c'from art import *; tprint("Python3")'
+check "python3 file" python3 "$(pwd)"/test.py
 
 reportResults
