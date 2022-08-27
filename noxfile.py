c9e8194a5fa491cf8ef6a560079e8cb2bfc94ab8
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-08-27 16:02:20 +0000

    ci: Add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Nox configuration file.
+"""
+import nox
+
+PYTHON_DEFAULT_VERSION = "3.10"
+PYTHON_ALL_VERSIONS = ["3.7", "3.8", "3.9", "3.10"]
+LINT_DEPENDENCIES = [
+    "isort",
+    "black",
+    "flake8",
+]
+
+
+@nox.session(name="isort", python=PYTHON_DEFAULT_VERSION)
+def isort(session):
+    """Run isort code formatter."""
+    session.install("isort")
+    session.run(
+        "isort",
+        "--profile",
+        "black",
+        ".",
+    )
+
+
+@nox.session(name="black", python=PYTHON_DEFAULT_VERSION)
+def blacken(session):
+    """Run black code formatter."""
+    session.install(
+        "black",
+    )
+    session.run(
+        "black",
+        "--check",
+        ".",
+    )
+
+
+@nox.session(name="flake8", python=PYTHON_DEFAULT_VERSION)
+def flake(session):
+    """Run black code formatter."""
+    session.install("flake8")
+    session.run(
+        "flake8",
+        ".",
+    )
+
+
+@nox.session(name="lint", python=PYTHON_ALL_VERSIONS)
+def lint(session):
+    """Run linters."""
+    session.install(*LINT_DEPENDENCIES)
+    session.run(
+        "isort",
+        "--profile",
+        "black",
+        ".",
+    )
+    session.run(
+        "black",
+        "--check",
+        "--verbose",
+        ".",
+    )
+    session.run(
+        "flake8",
+        ".",
+    )
