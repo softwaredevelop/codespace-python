a383bdff622f5d8d82defa94a5ddc30bb65e9fbb
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-11-22 01:40:34 +0100

    test: Update

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ SCRIPT_FOLDER_NAME=$(dirname $0)
 cd ${SCRIPT_FOLDER_NAME} || exit
 
 # shellcheck source=/dev/null
-source test-utils.sh codespace
+source test-utils.sh
 
 if [ "${ID_OS}" = "ubuntu" ]; then
   package_list_debian="apt-utils \
@@ -35,17 +35,5 @@ check "shfmt" shfmt --version
 check "shellcheck" shellcheck --version
 check "editorconfig" ec --version
 check "trivy" trivy --version
-if [ "${ID_OS}" = "ubuntu" ]; then
-  python_system_package_list_debian="python3 \
-        python3-dev \
-        python3-pip \
-        python3-venv"
-  checkOSPackages "python-system-packages" ${python_system_package_list_debian}
-fi
-check "python" python --version
-check "python3" python3 --version
-check "python3 ascii art" python3 -c'from art import *; tprint("Python3")'
-check "python3 file" python3 "$(pwd)"/test.py
-check "pipx directory" [ -d "/usr/local/pipx/bin" ] && echo "Pipx directory Exists"
 
 reportResults
